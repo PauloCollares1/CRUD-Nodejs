100367296f461e136ecc0f255a2d87d6d752c034
Pauloc1Dev <paulocollares1@hotmail.com>
2022-03-03 00:44:40 -0300

    iniciando a funcção update parte 2

--- Public/Files/files_scripts.js
+++ Public/Files/files_scripts.js
@@ -22,7 +22,7 @@ function entrega_valores(){
                             <b>Idade:</b>` + item.idade +
                             `<b>Email:</b>` + item.email +
                             `<button id='`+ id +`' onclick='deletar(${id})' value='`+item._id+`'> Deletar </button>
-                            <a href="/api/update"><button id='`+ id +`' onclick='teste2()' value='`+item._id+`'> Editar </button></a>
+                            <button id='`+ id +`' onclick='teste2()' value='`+item._id+`'> Editar </button>
                         </div>  
                     </div>`
                 )
@@ -57,5 +57,5 @@ function deletar(pega_id){
 function teste2(){
     const URL="http://uol.com.br";
     window.open(URL, 'janela', 'width=660, height=510, top=100, left=699,scrollbars=yes, status=no, toolbar=no, location=no, directories=no, menubar=no,resizable=no, fullscreen=no')
-
+    
 }
