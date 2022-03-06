ac900581ad90de66cb0fb8b5f1840a01bb86a878
Pauloc1Dev <paulocollares1@hotmail.com>
2022-03-06 17:58:18 -0300

    retirando localhost do fetch

--- Public/scripts.js
+++ Public/scripts.js
@@ -27,7 +27,7 @@ function pega_valores(){
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(credenciais)
     }
-    fetch('https://crud-in-node.herokuapp.com/api/cadastramento',options)
+    fetch('/api/cadastramento',options)
     document.getElementById('nome_html').value = "";
     document.getElementById('idade_html').value = "";
     document.getElementById('email_html').value = ""; 
